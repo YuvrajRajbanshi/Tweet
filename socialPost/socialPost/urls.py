95381dba30c0d38fd05cfc5e1c9706342c157278
YuvrajRajbanshi <singhraj0461@gmail.com>
2025-02-09 00:18:34 +0530

    Adding guiding page

--- socialPost/socialPost/urls.py
+++ socialPost/socialPost/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.guide, name='guide'),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
